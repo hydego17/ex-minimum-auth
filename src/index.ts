3f2f8e9634d47b3a0bb9679a7a7b1a3c679c032a
Umma Ahimsha <uahimsha@gmail.com>
2020-12-08 20:30:52 +0700

    init

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("hi there");
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Supppp");
+});
+
+app.listen(5000, () => {
+  console.log(`Server running on port 5000`);
+});
